84371cf3a1f35c001d1307ae67a55c6040d2bbca
Fernando <fernando452000@gmail.com>
2021-10-23 21:49:23 -0500

    Primera Versión

---
+++ app.js
@@ -0,0 +1,44 @@
+const img = document.querySelector('img');
+const hitSpan = document.querySelector('span');
+let hit = true;
+let hitScore = 0;
+
+function url() {
+    img.src = 'img/cheemsbu.png';
+}
+
+img.addEventListener('click',()=>{
+    if(hit === true){
+        img.src = 'img/cheemsbd.png';
+        hit = false;
+        hitScore++;
+        hitSpan.innerText = hitScore;
+    } else{
+        img.src = 'img/cheemsbu.png';
+        hit = true;
+    }
+});
+
+document.addEventListener('keydown',(e)=>{
+    console.log(e.code);
+    switch (e.code){
+        case 'ArrowDown':
+            img.src = 'img/cheemsbd.png';
+            setTimeout(url, 100);
+            hitScore++;
+            hitSpan.innerText = hitScore;
+        break;
+        case 'Enter':
+            img.src = 'img/cheemsbd.png';
+            setTimeout(url, 100);
+            hitScore++;
+            hitSpan.innerText = hitScore;
+        break;
+        case 'Space':
+            img.src = 'img/cheemsbd.png';
+            setTimeout(url, 100);
+            hitScore++;
+            hitSpan.innerText = hitScore;
+        break;
+    }
+});
