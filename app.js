c9721b4d54cf062a536860501623adb7173897fa
Fernando <fernando452000@gmail.com>
2021-10-24 13:55:43 -0500

    Mejora de click y sonido

--- app.js
+++ app.js
@@ -8,42 +8,59 @@ function url() {
     img.src = 'img/cheemsbu.png';
 }
 
+
+function pause(){
+    bonk.pause();
+    bonk.currentTime = 0;
+}
+
 img.addEventListener('click',()=>{
-    if(hit === true){
+    pause();
+    bonk.play();
+    img.src = 'img/cheemsbd.png';
+    setTimeout(url, 100);
+    hitScore++;
+    hitSpan.innerText = hitScore;
+
+/*     if(hit === true){
+        pause();
+        bonk.play();
         img.src = 'img/cheemsbd.png';
         hit = false;
         hitScore++;
         hitSpan.innerText = hitScore;
-        bonk.play();
     } else{
         img.src = 'img/cheemsbu.png';
         hit = true;
-    }
+    } */
 });
 
 document.addEventListener('keydown',(e)=>{
     console.log(e.code);
     switch (e.code){
         case 'ArrowDown':
+            pause();
+            bonk.play();
             img.src = 'img/cheemsbd.png';
             setTimeout(url, 100);
             hitScore++;
             hitSpan.innerText = hitScore;
-            bonk.play();
         break;
         case 'Enter':
+            pause();
+            bonk.play();
             img.src = 'img/cheemsbd.png';
             setTimeout(url, 100);
             hitScore++;
             hitSpan.innerText = hitScore;
-            bonk.play();
         break;
         case 'Space':
+            pause();
+            bonk.play();
             img.src = 'img/cheemsbd.png';
             setTimeout(url, 100);
             hitScore++;
             hitSpan.innerText = hitScore;
-            bonk.play();
         break;
     }
 });
